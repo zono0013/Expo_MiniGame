1c846b567836349368376382ed300d87d12ebbe8
仮屋薗純 <zono@zonos-Mac.local>
2024-07-10 13:15:50 +0900

    CameraTemplate

---
+++ Assets/Player/moves/character_move.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Transform controller;
+    private Transform CenterEyeAnchor;
+
+    private void Awake()
+    {
+        controller = GetComponent<Transform>();
+        // 特定の名前の孫オブジェクトを探す
+        CenterEyeAnchor = transform.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 stickR = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
+
+        // Y軸の回転角度を取得
+        float yRotation = CenterEyeAnchor.eulerAngles.y;
+
+        // 回転角度をラジアンに変換
+        float radians = -yRotation * Mathf.Deg2Rad;
+
+        // sinとcosを使用して移動方向を調整
+        float moveX = stickR.x * Mathf.Cos(radians) - stickR.y * Mathf.Sin(radians);
+        float moveZ = stickR.x * Mathf.Sin(radians) + stickR.y * Mathf.Cos(radians);
+
+        Vector3 movePostiion = new Vector3(moveX / 10, 0, moveZ / 10);
+        controller.Translate(movePostiion);
+    }
+}
+
